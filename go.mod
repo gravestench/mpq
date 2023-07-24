594f61e55cd826e9b4caa7f510f9ec2364c8b197
gravestench <dknuth0101@gmail.com>
2023-07-24 08:34:50 -0700

    fix go.mod, minor non-functional edits

--- go.mod
+++ go.mod
@@ -1,10 +1,13 @@
-module github.com/OpenDiablo2/mpq
+module github.com/gravestench/mpq
 
 go 1.17
 
 require (
 	github.com/JoshVarga/blast v0.0.0-20210808061142-eadad17358e8
+	github.com/OpenDiablo2/wav v0.0.0-20210822025314-429cc1291a8e
+)
+
+require (
 	github.com/OpenDiablo2/OpenDiablo2 v0.0.0-20210514222603-a688d660a0f7 // indirect
 	github.com/OpenDiablo2/bitstream v0.0.0-20210818234514-9fca7e40e2b3 // indirect
-	github.com/OpenDiablo2/wav v0.0.0-20210822025314-429cc1291a8e // indirect
 )
--- pkg/block.go
+++ pkg/block.go
@@ -5,32 +5,6 @@ import (
 	"strings"
 )
 
-// FileFlag represents flags for a file record in the MPQ archive
-type FileFlag uint32
-
-const (
-	// FileImplode - File is compressed using PKWARE Data compression library
-	FileImplode FileFlag = 0x00000100
-	// FileCompress - File is compressed using combination of compression methods
-	FileCompress FileFlag = 0x00000200
-	// FileEncrypted - The file is encrypted
-	FileEncrypted FileFlag = 0x00010000
-	// FileFixKey - The decryption key for the file is altered according to the position of the file in the archive
-	FileFixKey FileFlag = 0x00020000
-	// FilePatchFile - The file contains incremental patch for an existing file in base MPQ
-	FilePatchFile FileFlag = 0x00100000
-	// FileSingleUnit - Instead of being divided to 0x1000-bytes blocks, the file is stored as single unit
-	FileSingleUnit FileFlag = 0x01000000
-	// FileDeleteMarker - File is a deletion marker, indicating that the file no longer exists. This is used to allow patch
-	// archives to delete files present in lower-priority archives in the search chain. The file usually
-	// has length of 0 or 1 byte and its name is a hash
-	FileDeleteMarker FileFlag = 0x02000000
-	// FileSectorCrc - File has checksums for each sector. Ignored if file is not compressed or imploded.
-	FileSectorCrc FileFlag = 0x04000000
-	// FileExists - Set if file exists, reset when the file was deleted
-	FileExists FileFlag = 0x80000000
-)
-
 // Block represents an entry in the block table
 type Block struct { // 16 bytes
 	FilePosition         uint32
